4302fe317f20796a5e80bbc7a3d64717b1c1ff13
newstrangerX <=>
2023-09-11 09:54:31 +0100

    done

--- learn_static_libs/printf/include/header.h
+++
@@ -1,5 +0,0 @@
-#ifndef _HEADER_H
-#define _HEADER_H
-
-int prt(char *format, ...);
-#endif
--- learn_static_libs/printf/main.c
+++
@@ -1,7 +0,0 @@
-#include "include/header.h"
-
-int main()
-{
-    prt("Hello %G My Name Is %s And My Age Is %d osf soo %c hmm %c yeah", "asd", 18, '+','Y');
-}
-
