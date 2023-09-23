710b0bba4a3402ce95f56bd57ddb9478b0ff693a
newstrangerX <=>
2023-09-23 12:44:04 +0100

    done

--- cpp_test/LISTS_LEARN/main.cpp
+++ cpp_test/LISTS_LEARN/main.cpp
@@ -1,24 +1,24 @@
 #include<stdio.h>
 #include "lists.h"
-
+#define c (char*)
 int main()
 {
     nsX_LinkedList* list = new nsX_LinkedList();
-    list->nsX_AddNewItem("kkeeyy","vvaalueeSDAS>DASDP)(ASD())");
-    list->nsX_AddNewItem("key1","value1");
-    list->nsX_AddNewItem("keys1","value2");
-    list->nsX_AddNewItem("keys2","value3");
-    list->nsX_AddNewItem("key4","value4");
-    list->nsX_AddNewItem("key5","value5");
-    list->nsX_AddNewItem("key6","value6");
+    list->nsX_AddNewItem(c"kkeeyy",c"vvaalueeSDAS>DASDP)(ASD())");
+    list->nsX_AddNewItem(c"key1",c"value1");
+    list->nsX_AddNewItem(c"keys1",c"value2");
+    list->nsX_AddNewItem(c"keys2",c"value3");
+    list->nsX_AddNewItem(c"key4",c"value4");
+    list->nsX_AddNewItem(c"key5",c"value5");
+    list->nsX_AddNewItem(c"key6",c"value6");
 
-    list->PutValueInKey("keys1","valyoo");
+    list->PutValueInKey(c"keys1",c"valyoo");
     
     //list->PutValueInKey("keys1");
     // item *i2;
     // i2->key = "key2";
     // i2->value = "value2";
-    list->RemoveValueKey("key5");
+    list->RemoveValueKey(c"key5");
     // list->AddNewItem(i2);
     //printf("%s",list->GetValueInIndex(6));
     printf("%s",list->nsX_GetValueInIndex(5));
