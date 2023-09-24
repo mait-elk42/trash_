71d53d8ae48b2d2eb3dba88e6e1f32073ed3414f
newstrangerXD <newstrangerXD@gmail.com>
2023-09-24 23:53:46 +0100

    done

--- cpp_test/SFMLTEST/main.cpp
+++ cpp_test/SFMLTEST/main.cpp
@@ -2,11 +2,18 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(250, 500), "SFML works!");
-    GameObject* player = new GameObject(sf::CircleShape(10.f),125,250,1);
-    //clrterm;
+    sf::RenderWindow window(sf::VideoMode(600, 400), "SFML works!");
+    sf::Shape *box = new sf::RectangleShape(sf::Vector2f(50.0f,50.0f));
+    sf::Shape *circle = new sf::CircleShape(10.0);
+    GameObject* player = new GameObject(circle,200,100,1);
+    GameObject* ground = new GameObject(box,200,200,0);
+
+    circle->setFillColor(sf::Color::Red);
+    clrterm;
+    // sf::CircleShape ciiir = sf::CircleShape(10.0);
     while (window.isOpen())
     {
+        //player->__ObjectIsColliRecord(ground);
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -17,16 +24,29 @@ int main()
             if (event.type == sf::Event::KeyReleased)
                 player->keyreleased(event.key);
         }
+        //
+        // prt("player :");
+        // prt(player->getobj()->getPosition().x);
+        // prt("ground");
+        // prt(ground->getobj()->getPosition().x);
+        //graviiity
+            //player->move(0,0.2f);
+
         if(player->down)
-                player->move(0,1);
-            if(player->up)
-                player->move(0,-1);
-            if(player->left)
-                player->move(-1,0);
-            if(player->right)
-                player->move(1,0);
+            player->move(0,1);
+        if(player->up)
+            player->move(0,-1);                
+        if(player->left)
+            player->move(-1,0);
+        if(player->right)
+            player->move(1,0);
         window.clear();
-        window.draw(player->getobj());
+        window.draw(*(ground->getobj()));
+        window.draw(*(player->getobj()));
+
+        
+        player->printcollider(window);
+        ground->printcollider(window);
         window.display();
     }
 
--- cpp_test/SFMLTEST/src/GameObject.cpp
+++ cpp_test/SFMLTEST/src/GameObject.cpp
@@ -5,22 +5,88 @@ struct V2{
 };
 class GameObject{
 private:
+    sf::Vertex collider[8] =
+    {
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0)),
+        sf::Vertex(sf::Vector2f(0, 0))
+    };
+    int collition_down = 0 , collition_up = 0, collition_left = 0, collition_right = 0;
     float speed = 0;
-    sf::CircleShape shape;
+    sf::Shape *shape;
     V2 *position = new V2();
 public:
     int up = 0,down = 0,left = 0,right = 0;
-    GameObject(sf::CircleShape shape,float x,float y,float movementspeed)
+
+    GameObject(sf::Shape *shape,float x,float y,float movementspeed)
     {
+        collider[0].color = sf::Color::Green;
+        collider[1].color = sf::Color::Green;
+        collider[2].color = sf::Color::Green;
+        collider[3].color = sf::Color::Green;
+        collider[4].color = sf::Color::Green;
+        collider[5].color = sf::Color::Green;
+        collider[6].color = sf::Color::Green;
+        collider[7].color = sf::Color::Green;
+    
         this->shape = shape;
         position->x = x;
         position->y = y;
         speed = movementspeed;
-        this->shape.setPosition(position->x,position->y);
+        this->shape->setPosition(position->x,position->y);
     }
-    sf::CircleShape getobj()
+    sf::Shape *getobj()
     {
         return shape;
+    }
+    V2 getscale()
+    {
+        return {shape->getScale().x, shape->getScale().y};
+    }
+    V2 getPosition()
+    {
+        return {shape->getPosition().x, shape->getPosition().y};
+    }
+    // int ObjectIscollited()
+    // {
+    //     return collited;
+    // }
+    void printcollider(sf::RenderWindow& window)
+    {
+        collider[0].position = {getPosition().x, getPosition().y};
+        collider[1].position = {getPosition().x+20, getPosition().y};
+        collider[2].position = {getPosition().x+20, getPosition().y+20};
+        collider[3].position = {getPosition().x+20, getPosition().y};
+        collider[4].position = {getPosition().x+20, getPosition().y+20};
+        collider[5].position = {getPosition().x, getPosition().y+20};
+        collider[6].position = {getPosition().x, getPosition().y+20};
+        collider[7].position = {getPosition().x, getPosition().y};
+
+        window.draw(collider, 8, sf::Lines);
+    }
+    void __ObjectIsColliRecord(GameObject *othergameobject)
+    {
+        // if(getPosition().y+20 < othergameobject->getPosition().y)
+        //     collition_down = 1;
+        // else
+        //     collition_down = 0;
+        // if(getPosition().y > othergameobject->getPosition().y+50) 
+        //     collition_up = 1;
+        // else
+        //     collition_up = 0;
+        if(getPosition().x+20 < othergameobject->getPosition().x)
+            collition_right = 1;
+        else
+            collition_left = 0;
+        if(getPosition().x > othergameobject->getPosition().x+50)
+            collition_left = 1;
+        else
+            collition_right = 0;
     }
     void keypressed(sf::Event::KeyEvent keyclicked)
     {
@@ -50,12 +116,21 @@ public:
     }
     void set_color(sf::Color color)
     {
-        shape.setFillColor(color);
+        shape->setFillColor(color);
     }
     void move(float x,float y)
     {
+        // std::cout << " right: "<<collition_right <<  " left: " <<collition_left << std::endl;
+        // if(x > 0 && !collition_right)
+        //     position->x += x * speed;
+        // else
+        //     if(x < 0)
+        //         position->x += x * speed;
+        
+        // if(x < 0 && !collition_left)
+            //position->x += x * speed;
         position->x += x * speed;
         position->y += y * speed;
-        shape.setPosition(position->x,position->y);
+        shape->setPosition(position->x,position->y);
     }
 };
